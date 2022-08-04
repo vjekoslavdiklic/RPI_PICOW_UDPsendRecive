f08323fb77faf07b1e4ec95a66e1f99eaacc1991
Vjekoslav Diklic <vjekoslav.diklic@gmail.com>
2022-08-04 22:28:10 +0200

    Added GUI with ADC readout and LED Control

--- PCSIDE_udpsendrec.py
+++ PCSIDE_udpsendrec.py
@@ -1,6 +1,7 @@
-import socket
-import time
-
+import socket,time
+import tkinter as tk
+from tkinter import ttk
+import numpy as np
 #configuration settings
 Target_UDP_IP = "192.168.5.22"
 Local_UDP_IP = socket.gethostbyname(socket.gethostname()) #Obtain automaticaly
@@ -9,15 +10,51 @@ BufferSize=1024
 MESSAGE = b"B\n" #or b"B\n" for OFF
 TimeOutInSec=0.5
 
+def LedON():
+    global MESSAGE
+    MESSAGE = b"A\n"
+
+def LedOFF():
+    global MESSAGE
+    MESSAGE = b"B\n"
+
 #initialize
 SockOut = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP socket for sending data
 SockIn = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP socket for receiving data
 SockIn.bind((Local_UDP_IP , UDP_PORT)) #bing to local ip and socket
 
-# do in inf. loop send receive
-while True:
+#windows settings:
+window = tk.Tk()
+window.geometry('300x120')
+window.title('UDP_Readout&Control')
+window.grid()
+
+#progress bar settings:
+pb = ttk.Progressbar(window,orient='horizontal',mode='determinate',length=280)
+pb.grid(column=0, row=0, columnspan=2, padx=10, pady=20)
+start_button = ttk.Button(window,text='Turn ON',command=LedON)
+start_button.grid(column=0, row=1, padx=10, pady=10, sticky=tk.E)
+stop_button = ttk.Button(window,text='Turn OFF',command=LedOFF)
+stop_button.grid(column=1, row=1, padx=10, pady=10, sticky=tk.W)
+
+#UDP communication function:
+def updateudpdata():
+    global MESSAGE
     SockOut.sendto(MESSAGE, (Target_UDP_IP, UDP_PORT)) #send data
     print("UDP message has been send")
     data, addr = SockIn.recvfrom(BufferSize)  #recive data
     print("received message: %s" % data)
-    time.sleep(TimeOutInSec) #wait for timeout
+    a = str(data)
+    b = a.split('A=')
+    c = b[1]
+    c = c[0:-1]
+    c = int(c)
+    d = (c / 1023) * 100
+    d = int(d)
+    pb['value'] = d
+
+#main loop
+while True:
+    window.after(0, updateudpdata)
+    window.update()
+window.mainloop()
